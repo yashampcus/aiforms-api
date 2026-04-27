1142834d75af24c3310df886fa8a349ac1b42f5b
yashampcus <yash.waikar@ampcus.com>
2026-04-27 10:52:16 -0400

    chore: nodejs fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { webcrypto } from 'node:crypto'
+if (!globalThis.crypto) (globalThis as any).crypto = webcrypto
+
 import express, { type Request, type Response, type NextFunction } from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
